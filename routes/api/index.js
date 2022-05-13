2adf258564fec4b14ff16be72b49d037ed3e4021
Hatchwald <fatkhur139wec@gmail.com>
2022-05-13 22:38:58 +0700

    paginate api , added body-parser

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,13 +2,21 @@ var express = require('express')
 var router = express.Router()
 const axios = require('axios')
 
-router.get("/", async function (req, res, next) {
+router.get("/", function (req, res, next) {
+    // const params = req.params
+    console.log(req.query.limit);
     axios
-        .get('https://pokeapi.co/api/v2/pokemon')
+        .get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
         .then(result => {
-            res.status(result.status).json(result.data)
-            // console.log(`statusCode: ${result.status}`);
-            // console.log(result.data);
+            const raw_data = result.data.results
+            let limit = 20
+            let offset = 0
+            let final_data = []
+            for (let index = offset; index < limit; index++) {
+                final_data.push(raw_data[index])
+            }
+            const result_data = { limit: limit, data: final_data }
+            res.status(result.status).json(result_data)
         })
         .catch(error => {
             console.error(error);
